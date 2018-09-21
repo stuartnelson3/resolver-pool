74797ee8bfde1bfcf1c0a329da006753fcc9a64e
stuart nelson <stuartnelson3@gmail.com>
2018-09-21 15:27:13 +0200

    Add run loop for refreshing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate crossbeam;
+
+use crossbeam::channel;
 use std::net::SocketAddr;
 use std::string::String;
+use std::thread;
 use std::time::Duration;
 
 pub trait Resolver {
@@ -10,7 +15,15 @@ pub struct Error(String);
 
 pub struct ResolverPool<R> {
     resolver: R,
+    // Refresh interval in seconds.
     refresh_interval: Duration,
+    state: State,
+    stopc: channel::Sender<usize>,
+}
+
+enum State {
+    Initialized,
+    Running,
 }
 
 impl<R> ResolverPool<R>
@@ -18,10 +31,37 @@ where
     R: Resolver,
 {
     pub fn new(resolver: R, refresh_interval: Duration) -> Self {
+        let (stopc, _) = channel::bounded(0);
         ResolverPool {
             resolver: resolver,
             refresh_interval: refresh_interval,
+            state: State::Initialized,
+            stopc: stopc,
         }
+    }
+
+    pub fn run(&mut self) -> Result<(), Error> {
+        match self.state {
+            State::Running => return Err(Error("already running".to_owned())),
+            State::Initialized => {}
+        };
+        self.state = State::Running;
+
+        let (stopc, r) = channel::bounded(0);
+        self.stopc = stopc;
+        let refreshc = channel::tick(self.refresh_interval);
+        thread::spawn(move || loop {
+            select!{
+                recv(refreshc) => {
+                },
+                recv(r) => return
+            }
+        });
+        Ok(())
+    }
+
+    pub fn stop(mut self) {
+        drop(self.stopc)
     }
 }
 
