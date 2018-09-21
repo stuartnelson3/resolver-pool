d479a30740f85d75cbc8d47f312be324842fea04
stuart nelson <stuartnelson3@gmail.com>
2018-09-21 13:53:59 +0200

    skeleton for pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+use std::net::SocketAddr;
+use std::string::String;
+use std::time::Duration;
+
+pub trait Resolver {
+    fn resolve<'a>(key: &'a str) -> Result<Vec<SocketAddr>, Error>;
+}
+
+pub struct Error(String);
+
+pub struct ResolverPool<R> {
+    resolver: R,
+    refresh_interval: Duration,
+}
+
+impl<R> ResolverPool<R>
+where
+    R: Resolver,
+{
+    pub fn new(resolver: R, refresh_interval: Duration) -> Self {
+        ResolverPool {
+            resolver: resolver,
+            refresh_interval: refresh_interval,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
