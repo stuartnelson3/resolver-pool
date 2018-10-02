68458fed5690e1dd2883dad4ba1ce26708bebd5c
stuart nelson <stuartnelson3@gmail.com>
2018-10-02 15:19:33 +0200

    Add Arc to run refresh in other thread

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,19 @@ use crossbeam::queue::MsQueue;
 use std::marker::{Send, Sync};
 use std::net::SocketAddr;
 use std::string::String;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 pub trait Resolver {
-    fn resolve<'a>(&self, key: &'a str) -> Result<Vec<SocketAddr>, Error>;
+    fn resolve(&self) -> Result<Vec<SocketAddr>, Error>;
 }
 
 #[derive(Debug)]
 pub struct Error(String);
 
 pub struct ResolverPool<R> {
-    resolver: R,
+    resolver: Arc<R>,
     // Refresh interval in seconds.
     refresh_interval: Duration,
     state: State,
@@ -28,7 +29,7 @@ pub struct ResolverPool<R> {
     // "timeout" for bad SocketAddr's, and re-try them after a configurable timeout. e.g., the last
     // X connections have failed, or the avg latency for the last Y connections is above a
     // threshold, put it into a future that will add it back to the queue after Z milliseconds.
-    cache: MsQueue<SocketAddr>,
+    cache: Arc<MsQueue<SocketAddr>>,
 }
 
 enum State {
@@ -38,59 +39,64 @@ enum State {
 
 impl<R> ResolverPool<R>
 where
-    R: Resolver + Send + Sync,
+    R: 'static + Resolver + Send + Sync,
 {
     pub fn new(resolver: R, refresh_interval: Duration) -> Self {
         let (stopc, _) = channel::bounded(0);
         ResolverPool {
-            resolver: resolver,
+            resolver: Arc::new(resolver),
             refresh_interval: refresh_interval,
             state: State::Initialized,
             stopc: stopc,
-            cache: MsQueue::new(),
+            cache: Arc::new(MsQueue::new()),
         }
     }
 
     pub fn get(&mut self) -> Option<SocketAddr> {
-        let addr = self.cache.try_pop();
-        if let Some(addr) = addr {
-            self.cache.push(addr);
-        }
-        addr
+        let addr = self.cache.try_pop()?;
+        self.cache.push(addr);
+        Some(addr)
     }
 
-    pub fn run<'a>(&mut self, key: &'a str) -> Result<(), Error> {
+    pub fn run(&mut self) -> Result<(), Error> {
         match self.state {
             State::Running => return Err(Error("already running".to_owned())),
             State::Initialized => {}
         };
         self.state = State::Running;
 
-        for addr in self.resolver.resolve(key)? {
+        while !self.cache.is_empty() {
+            self.cache.pop();
+        }
+
+        for addr in self.resolver.resolve()? {
             self.cache.push(addr);
         }
 
         let (stopc, r) = channel::bounded(0);
         self.stopc = stopc;
         let refreshc = channel::tick(self.refresh_interval);
+        let resolver = Arc::clone(&self.resolver);
+        let cache = Arc::clone(&self.cache);
         thread::spawn(move || loop {
             select!{
                 recv(refreshc) => {
-                    // match self.resolver.resolve(key) {
-                        // Ok(addrs) => {
-                        // if addrs.is_empty() {
-                        //     continue
-                        // }
-                        // while !self.cache.is_empty() {
-                        //     self.cache.pop();
-                        // }
-                        // for addr in addrs {
-                        //     self.cache.push(addr);
-                        // }
-                        // Err(err) => print!("failed to refresh: {:?}", err),
-                    // }
+                    match resolver.resolve() {
+                        Ok(addrs) => {
+                            if addrs.is_empty() {
+                                continue
+                            }
+                            while !cache.is_empty() {
+                                cache.pop();
+                            }
+                            for addr in addrs {
+                                cache.push(addr);
+                            }
+                        }
+                        Err(err) => print!("failed to refresh: {:?}", err),
+                    }
                 },
-                recv(r.clone()) => return
+                recv(r) => return
             }
         });
         Ok(())
