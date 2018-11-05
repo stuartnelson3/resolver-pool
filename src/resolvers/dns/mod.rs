56f2b6e6d92987fe5810c22c9f2d2b393b60690c
stuart nelson <stuartnelson3@gmail.com>
2018-11-05 19:05:49 +0100

    fallback to TCP lookup

--- src/resolvers/dns/mod.rs
+++ src/resolvers/dns/mod.rs
@@ -1,55 +1,73 @@
 extern crate trust_dns;
 
 use resolvers::dns::trust_dns::client::Client;
+use resolvers::dns::trust_dns::client::SyncClient;
 use resolvers::dns::trust_dns::op::DnsResponse;
 use resolvers::dns::trust_dns::rr::{DNSClass, Name, RData, Record, RecordType};
+use resolvers::dns::trust_dns::tcp::TcpClientConnection;
+use resolvers::dns::trust_dns::udp::UdpClientConnection;
 use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 use Error;
 use Resolver;
 
-pub struct TrustDNS<T> {
-    client: T,
-    lookup: &'static str,
+pub struct TrustDNS {
+    pub udp_client: SyncClient<UdpClientConnection>,
+    pub tcp_client: SyncClient<TcpClientConnection>,
+    pub lookup: &'static str,
 }
 
-impl<T> TrustDNS<T>
-where
-    T: Client,
-{
+impl TrustDNS {
     // TODO: Inject Prometheus registry.
-    pub fn new(client: T, lookup: &'static str) -> Self {
-        TrustDNS { client, lookup }
+    pub fn new(name_server: SocketAddr, lookup: &'static str) -> Self {
+        let udp_client = SyncClient::new(UdpClientConnection::new(name_server).unwrap());
+        let tcp_client = SyncClient::new(TcpClientConnection::new(name_server).unwrap());
+        TrustDNS {
+            udp_client,
+            tcp_client,
+            lookup,
+        }
     }
 }
 
-impl<T> Resolver<SocketAddr> for TrustDNS<T>
-where
-    T: Client,
-{
+impl Resolver<SocketAddr> for TrustDNS {
     fn resolve(&self) -> Result<Vec<SocketAddr>, Error> {
         let name = Name::from_str(self.lookup).expect("failed to create name");
         // .map_err(|err| Err(Error("TODO: Map error".to_string())))?;
 
-        let response: DnsResponse = self
-            .client
+        let mut response: DnsResponse = self
+            .udp_client
             .query(&name, DNSClass::IN, RecordType::SRV)
             .expect("failed to query client");
         // .map_err(|err| Err(Error("TODO: Map error".to_string())))?;
 
-        let answers: &[Record] = response.answers();
+        if response.truncated() {
+            response = self
+                .tcp_client
+                .query(&name, DNSClass::IN, RecordType::SRV)
+                .expect("failed to query client");
+        }
 
-        let srv_records = answers
+        let srv_records = response
+            .answers()
             .iter()
             .map(|a| a.rdata())
             .filter_map(|srv| match srv {
                 &RData::SRV(ref srv) => {
-                    let response: DnsResponse = self
-                        .client
+                    let mut response: DnsResponse = self
+                        .udp_client
                         .query(&srv.target(), DNSClass::IN, RecordType::A)
                         .ok()?;
                     // Log failure
                     // .expect("A record lookup failed");
+
+                    if response.truncated() {
+                        response = self
+                            .udp_client
+                            .query(&srv.target(), DNSClass::IN, RecordType::A)
+                            .ok()?;
+                    }
+
                     let answer: &Record = response.answers().first()?;
                     // Log failure
                     if let &RData::A(ip) = answer.rdata() {
