67ef2c07a34dfffd0cf3ba6af99ab1b50b3f30b2
stuart nelson <stuartnelson3@gmail.com>
2018-11-05 18:41:38 +0100

    Perform A lookup on returned CNAMEs
    Perform an A lookup on each CNAME returned from
    the initial SRV lookup.

--- src/resolvers/dns/mod.rs
+++ src/resolvers/dns/mod.rs
@@ -3,7 +3,7 @@ extern crate trust_dns;
 use resolvers::dns::trust_dns::client::Client;
 use resolvers::dns::trust_dns::op::DnsResponse;
 use resolvers::dns::trust_dns::rr::{DNSClass, Name, RData, Record, RecordType};
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 use Error;
 use Resolver;
@@ -34,23 +34,33 @@ where
         let response: DnsResponse = self
             .client
             .query(&name, DNSClass::IN, RecordType::SRV)
-            .expect("failed to query client"); // Make record type configurable?
-                                               // .map_err(|err| Err(Error("TODO: Map error".to_string())))?;
+            .expect("failed to query client");
+        // .map_err(|err| Err(Error("TODO: Map error".to_string())))?;
 
         let answers: &[Record] = response.answers();
 
-        let srv_records: Vec<&RData> = answers.iter().map(|a| a.rdata()).collect();
-
-        srv_records
+        let srv_records = answers
             .iter()
+            .map(|a| a.rdata())
             .filter_map(|srv| match srv {
                 &RData::SRV(ref srv) => {
-                    let ip = srv.target()[0].to_utf8();
-                    let ip = ip.replace("-", "."); // TODO: Don't assume first target is ip addr.
-                    Some(format!("{}:{}", ip, srv.port()))
+                    let response: DnsResponse = self
+                        .client
+                        .query(&srv.target(), DNSClass::IN, RecordType::A)
+                        .ok()?;
+                    // Log failure
+                    // .expect("A record lookup failed");
+                    let answer: &Record = response.answers().first()?;
+                    // Log failure
+                    if let &RData::A(ip) = answer.rdata() {
+                        Some(SocketAddr::new(IpAddr::V4(ip), srv.port()))
+                    } else {
+                        // Log failure.
+                        None
+                    }
                 }
                 _ => None,
-            }).map(|ip| ip.parse().map_err(|_| Error("".to_string())))
-            .collect()
+            }).collect();
+        Ok(srv_records)
     }
 }
